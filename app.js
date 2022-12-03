c630e4ba7b8ebf07da8ef3db77ec3518ecccfae4
Alvaro8317 <eduardo831_@hotmail.com>
2022-12-03 13:22:03 -0500

    feat: A little of managing to the repository, added the configuration of the environment file and package.json

---
+++ app.js
@@ -0,0 +1,31 @@
+// const openai = require("openai");
+// const { apiKey, secret } = require("./APIGPT/config");
+
+// openai.apiKey = apiKey;
+// openai.secret = secret;
+
+// // Consume la API de GPT-3
+// openai.completions.create(
+//     {
+//         model: "MODELO_DE_GPT-3",
+//         prompt: "TU_PROMPT_O_TEXTO_DE_ENTRADA",
+//         max_tokens: NUMERO_DE_TOKENS_DESEADOS,
+//         n: NUMERO_DE_COMPLETACIONES_DESEADAS,
+//         stop: "TOKEN_DE_PARADA",
+//     },
+//     (error, response) => {
+//         if (error) {
+//             console.error(error);
+//         } else {
+//             // Procesa la respuesta de la API
+//             console.log(response);
+//         }
+//     }
+// );
+
+const {Configuration, OpenAIApi} = require("openai");
+require("dotenv").config();
+
+const instanceConfiguration = new Configuration({
+    apiKey: process.env.APIKEYOPENAI,
+})
