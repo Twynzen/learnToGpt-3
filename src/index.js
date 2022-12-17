d2b6b7ac544b6519179d47e21956b28d92d8cdaf
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2022-12-17 09:14:14 -0500

    test: call to api

--- src/index.js
+++ src/index.js
@@ -17,21 +17,21 @@ const main = async() => {
 
     // se puede sacar en un archivo aparte
     //De esta manera obtenemos una lista de los motores disponibles en la API
-    const engines = await openai.listEngines();
-    const enginesList = engines.data;
-    const enginesIds = enginesList.data.map(engine => engine.id);
-    enginesIds.forEach(id => console.log(id));
+    // const engines = await openai.listEngines();
+    // const enginesList = engines.data;
+    // const enginesIds = enginesList.data.map(engine => engine.id);
+    // enginesIds.forEach(id => console.log(id));
 
 
     // se puede sacar en un archivo aparte 
-    // const response = await openai.createCompletion({
-    //     prompt: prompt,
-    //     model: "text-davinci-002",
-    //     max_tokens: 100,
-    //     temperature: 0.5,
-    // })
+    const response = await openai.createCompletion({
+        prompt: prompt,
+        model: "text-davinci-002",
+        max_tokens: 100,
+        temperature: 0.5,
+    })
 
-    // console.log(response);
+    console.log(response);
 
 
 
